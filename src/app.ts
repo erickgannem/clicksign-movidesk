b73b50a5076ea95cc2e46ea5ed8c205f0c04a722
Erick Gannem <erickgannem@gmail.com>
2021-07-08 08:55:24 -0400

    add corresponding .env file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express, { Application } from 'express'
+import dotenv from 'dotenv'
 import routes from './routes'
+
+const { NODE_ENV } = process.env
 
 export default class App {
   server: Application
   constructor () {
+    switch (NODE_ENV) {
+      case 'dev':
+        dotenv.config({ path: '.env.dev' })
+        break
+      case 'prod':
+        dotenv.config({ path: '.env.prod' })
+        break
+      default:
+        dotenv.config()
+    }
     this.server = express()
     this.middlewares()
     this.routes()
