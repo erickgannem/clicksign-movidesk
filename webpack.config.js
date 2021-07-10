9bad589d36a3e02c5bf323576174e3d5097b8a61
Erick Gannem <erickgannem@gmail.com>
2021-07-10 05:40:09 -0400

    add and setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'none',
+  target: 'node',
+  entry: './src/server.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+}
