04abe08e64201c4fa2d1fc221c09b436f7372616
Erick Gannem <erickgannem@gmail.com>
2021-07-12 17:12:43 -0400

    destructuring to avoid acessing same object multiple times

--- src/handlers/verifyPerson.ts
+++ src/handlers/verifyPerson.ts
@@ -9,25 +9,38 @@ type CreatePersonOpts = {
   token: string
 }
 function _createPerson<T> ({ userData, token }: CreatePersonOpts) {
+  const {
+    cpf,
+    cep,
+    estado: state,
+    cidade: city,
+    bairro: district,
+    logradouro: street,
+    número: number,
+    'nome completo': fullName,
+    'e-mail signatário': email
+  } = userData
+
   return movidesk.post<T>(`/persons?token=${token}&returnAllProperties=true`, {
+    id: cpf,
     isActive: true,
     personType: 1,
     profileType: 2,
-    businessName: userData['nome completo'],
-    cpfCnpj: userData.cpf,
+    businessName: fullName,
+    cpfCnpj: cpf,
     addresses: [{
       addressType: 'Residencial',
-      postalCode: userData.cep,
-      state: userData.estado,
-      city: userData.cidade,
-      district: userData.bairro,
-      street: userData.logradouro,
-      number: userData['número'],
+      postalCode: cep,
+      state,
+      city,
+      district,
+      street,
+      number,
       isDefault: true
     }],
     emails: [{
+      email,
       emailType: 'Pessoal',
-      email: userData['e-mail signatário'],
       isDefault: true
     }],
     observations: `Created by script on ${new Date()}`
