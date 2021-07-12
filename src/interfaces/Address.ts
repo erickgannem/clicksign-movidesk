115306833a2c7aa07b33f8d90c15dc6f51449366
Erick Gannem <erickgannem@gmail.com>
2021-07-12 11:16:28 -0400

    Address interface

---
+++ src/interfaces/Address.ts
@@ -0,0 +1,12 @@
+export default interface Address {
+  addressType: string
+  country: string
+  state: string
+  city: string
+  district: string
+  street: string
+  number: string
+  complement: string
+  reference: string
+  isDefault: boolean
+}
