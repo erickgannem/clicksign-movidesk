732967ae607b704578a864b9021d670f2538b196
Erick Gannem <erickgannem@gmail.com>
2021-07-12 05:46:16 -0400

    interface to type

--- src/utils/createHash.ts
+++ src/utils/createHash.ts
@@ -1,14 +1,12 @@
 import { createHmac } from 'crypto'
 
-interface Opts {
+type Opts = {
   encoding: string,
   secretKey: string,
   payload: string | Buffer
 }
 
-export default function createHash (opts: Opts) {
-  const { encoding, secretKey, payload } = opts
-
+export default function createHash ({ encoding, secretKey, payload }: Opts) {
   const _hmac = createHmac(encoding, secretKey)
   _hmac.update(payload)
   const _hash = _hmac.digest('hex')
