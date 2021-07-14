6414bd30254be13ddf70a9d4c1bfcb117882cc4e
Erick Gannem <erickgannem@gmail.com>
2021-07-14 05:24:26 -0400

    create ticket request

--- src/handlers/createTicket.ts
+++ src/handlers/createTicket.ts
@@ -1,11 +1,46 @@
 import { Request, Response, NextFunction } from 'express'
+import movidesk from '@api/movidesk'
+import Person from '@interfaces/Person'
+import Ticket from '@interfaces/Ticket'
 
-const _createTicket = async () => {
-
+type CreateTicketOpts = {
+  person: Person
+  token: string
+  adminId: string
+}
+const _createTicketCall = async <T>({ person, token, adminId }: CreateTicketOpts) => {
+  const { id } = person
+  const ticket = movidesk.post<T>('/tickets?token=' + token, {
+    type: 2,
+    subject: '[TESTE] Assunto do Ticket via REST',
+    status: 'Novo',
+    origin: 8, // contact form
+    owner: null,
+    ownerTeam: null,
+    clients: [{ id }],
+    createdBy: { id: adminId },
+    actions: [{
+      description: 'Teste from REST CLIENT \n',
+      htmlDescription: '<p>Teste FROM REST CLIENT</p>',
+      type: 1 // intern
+    }]
+  })
+  return ticket
 }
 export default async function createTicket (req: Request, res: Response, next: NextFunction) {
-  const { person } = req
-  console.log(person)
-  // Proceed to create a ticket
-  return res.status(200).end()
+  try {
+    const { MOVIDESK_TOKEN, MOVIDESK_ADMIN_ID } = process.env
+    const { person } = req
+
+    process.stdout.write('Creating ticket...')
+    const ticket = await _createTicketCall<Ticket>({
+      person,
+      token: MOVIDESK_TOKEN as string,
+      adminId: MOVIDESK_ADMIN_ID as string
+    })
+
+    return res.status(200).json(ticket)
+  } catch (err) {
+    next(err)
+  }
 }
--- src/handlers/verifyPerson.ts
+++ src/handlers/verifyPerson.ts
@@ -67,10 +67,15 @@ export default async function verifyPerson (req: Request, res: Response, next: N
     })
 
     if (!retrieved.length) {
-      const person = await _createPerson<Person>({ userData, token: MOVIDESK_TOKEN as string })
+      process.stdout.write('Person does not exists. Creating a person...')
+      const person = await _createPerson<Person>({
+        userData,
+        token: MOVIDESK_TOKEN as string
+      })
       req.person = person.data
       return next()
     }
+    process.stdout.write('Person exists. Skipping...')
     req.person = retrieved[0]
     return next()
   } catch (err) {
